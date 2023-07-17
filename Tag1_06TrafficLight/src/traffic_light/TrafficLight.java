f5d1701c9d24e4a94f424d6a70174cc543aaa570
Jo Wagner <jowagner@limago.de>
2023-07-17 16:04:27 +0200

    initial commit

--- Tag1_06TrafficLight/src/traffic_light/TrafficLight.java
+++ Tag1_06TrafficLight/src/traffic_light/TrafficLight.java
@@ -1,11 +1,97 @@
 package traffic_light;
 public class TrafficLight {
 
+    private final TrafficLightState redState = new RedState();
+    private final TrafficLightState greenState = new GreenState();
+    private final TrafficLightState offState = new OffState();
+
+    private TrafficLightState current = offState;
+
     public String getColor() {
-        return null;
+        return current.getColor();
     }
 
     public void nextColor() {
+        current.nextColor();
+    }
 
+    public void switchOn() {
+        current.switchOn();
+    }
+
+    public void switchOff() {
+        current.switchOff();
+    }
+
+    interface TrafficLightState {
+        public String getColor() ;
+        public void nextColor();
+        public void switchOn();
+        public void switchOff();
+    }
+
+    class AbstractTrafficLightState implements TrafficLightState {
+
+        @Override
+        public String getColor() {
+            throw new UnsupportedOperationException("Upps");
+        }
+
+        @Override
+        public void nextColor() {
+            throw new UnsupportedOperationException("Upps");
+        }
+
+        @Override
+        public void switchOn() {
+            throw new UnsupportedOperationException("Upps");
+        }
+
+        @Override
+        public void switchOff() {
+            throw new UnsupportedOperationException("Upps");
+        }
+    }
+
+    class AbstractTrafficLightColorState extends AbstractTrafficLightState{
+        @Override
+        public void switchOff() {
+            TrafficLight.this.current = TrafficLight.this.offState;
+        }
+    }
+
+    class OffState extends AbstractTrafficLightState {
+
+        @Override
+        public void switchOn() {
+            TrafficLight.this.current = TrafficLight.this.redState;
+        }
+    }
+
+    class RedState extends AbstractTrafficLightColorState {
+
+        @Override
+        public String getColor() {
+            return "RED";
+        }
+
+        @Override
+        public void nextColor() {
+            TrafficLight.this.current = TrafficLight.this.greenState;
+        }
+    }
+
+    class GreenState extends AbstractTrafficLightColorState {
+
+
+        @Override
+        public String getColor() {
+            return "GREEN";
+        }
+
+        @Override
+        public void nextColor() {
+            TrafficLight.this.current = TrafficLight.this.redState;
+        }
     }
 }
