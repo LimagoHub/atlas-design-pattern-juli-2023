17abe4fb3424ec8a695885d26d69e41668366018
Jo Wagner <jowagner@limago.de>
2023-07-18 14:20:10 +0200

    initial commit

---
+++ Tag2_04Memento/src/de/tiere/Schwein.java
@@ -0,0 +1,77 @@
+package de.tiere;
+
+
+import java.util.ArrayList;
+
+public class Schwein {
+
+
+
+
+    private String name;
+    private int gewicht;
+
+    public Schwein(final String name) {
+        this.name = name;
+        this.gewicht = 10;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public int getGewicht() {
+        return gewicht;
+    }
+
+    private void setGewicht(final int gewicht) {
+
+        this.gewicht = gewicht;
+
+    }
+
+    public void fuettern() {
+        setGewicht(getGewicht() + 1);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Schwein{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", gewicht=").append(gewicht);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public SchweineMemento getMemento() {
+        return new MySchweineMemento(name, gewicht);
+    }
+
+    public void setMemento(SchweineMemento memento) {
+        MySchweineMemento mySchweineMemento = (MySchweineMemento)  memento;
+        setName(mySchweineMemento.getName());
+        setGewicht(mySchweineMemento.getGewicht());
+    }
+
+    private static class MySchweineMemento implements SchweineMemento {
+        private final String name;
+        private final int gewicht;
+
+        public MySchweineMemento(final String name, final int gewicht) {
+            this.name = name;
+            this.gewicht = gewicht;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getGewicht() {
+            return gewicht;
+        }
+    }
+}
